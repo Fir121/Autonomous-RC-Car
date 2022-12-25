2f29b259843ff23fa2021f26d677c536a77e209c
Fir121 <mohamedfiras2003@gmail.com>
2022-12-25 21:16:22 +0400

    vid processing

---
+++ backend.py
@@ -0,0 +1,25 @@
+import cv2 
+import numpy as np 
+
+def bw_conv(frame):
+    imgHsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    kernel_size = 5
+    imgHsv = cv2.GaussianBlur(imgHsv, (kernel_size, kernel_size), 0)
+    lowerWhite = np.array([0,0,135])
+    upperWhite = np.array([179,255,255])
+    maskWhite = cv2.inRange(imgHsv, lowerWhite, upperWhite)
+
+    return maskWhite
+
+def warp_img(frame):
+    h,w,c = frame.shape
+    points = [[w//6,0], [w-(w//6),0], [0,h], [w,h]]
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0,0], [w,0], [0,h], [w,h]])
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgWarp = cv2.warpPerspective(frame, matrix, (w,h))
+
+    return imgWarp
+
+def process_direction(frame):
+    return "NA"
---
+++ mytry.py
@@ -0,0 +1,30 @@
+import cv2 
+import numpy as np 
+from backend import *
+import time
+
+'''
+cap = cv2.VideoCapture(1)
+while True:
+    success,img = cap.read()
+    cv2.imshow("mainvid",img)
+    bw_conv(img)
+    cv2.waitKey(1)
+'''
+
+image = cv2.imread('asd.jpg')
+cv2.namedWindow('image')
+outp = bw_conv(image)
+outp2 = warp_img(image)
+outp3 = bw_conv(outp2)
+todo = process_direction(outp3)
+print(todo)
+while True:
+    time.sleep(0.00001)
+    cv2.imshow('image',outp)
+    cv2.imshow('wrped',outp2)
+    cv2.imshow('wrpedbw',outp3)
+
+    cv2.waitKey(1)
+
+cv2.destroyAllWindows()
