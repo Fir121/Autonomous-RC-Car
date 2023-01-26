8336ee0bcbe1c5393f582344be9049890cbdeb04
Fir121 <mohamedfiras2003@gmail.com>
2023-01-27 00:42:43 +0400

    adding to last

--- localisation.py
+++ localisation.py
@@ -1,4 +1,7 @@
-from Car import Car
+try:
+    from Car import Car
+except:
+    pass
 import math
 import time
 # USE MYTRY.PY TO ADJUST YOURSELF WITHIN THE LANE, HAVE PARAMETERS TO BE TWEAKED FOR MAIN TRACK. LATER MAKE PARAMS CENTRAL AND DUALS
@@ -11,7 +14,7 @@ try:
 except:
     pass
 
-car.turn((max_steer/math.degrees(math.atan(W/map_radius)))*100)
+car.turn((math.degrees(math.atan(W/map_radius))/max_steer)*100)
 car.move()
 
 while True:
@@ -20,14 +23,14 @@ while True:
 '''
 # OTHER PATH DEPENDING ON TRAFFIC SIGNAL RECOGNITION
 map_radius = 1.8
-car.turn((max_steer/math.degrees(math.atan(W/map_radius)))*100)
+car.turn((math.degrees(math.atan(W/map_radius))/max_steer)*100)
 time.sleep(4)
 map_radius = 1
-car.turn(-1*((max_steer/math.degrees(math.atan(W/map_radius)))*100))
+car.turn(-1*((math.degrees(math.atan(W/map_radius))/max_steer)*100))
 time.sleep(4)
 map_radius = 1.8
-car.turn((max_steer/math.degrees(math.atan(W/map_radius)))*100)
+car.turn((math.degrees(math.atan(W/map_radius))/max_steer)*100)
 time.sleep(4)
 map_radius = 1.3
-car.turn((max_steer/math.degrees(math.atan(W/map_radius)))*100)
+car.turn((math.degrees(math.atan(W/map_radius))/max_steer)*100)
 '''
