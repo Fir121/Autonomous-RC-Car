d20e62046f6d5e345ff45fff77b5b6d3d728da64
Fir121 <mohamedfiras2003@gmail.com>
2023-02-03 04:48:44 +0400

    added ultrasonic

--- wreclocal.py
+++ wreclocal.py
@@ -8,22 +8,22 @@ import cv2
 from backend import *
 import time
 from picamera2 import Picamera2
-try:
-    from Car import Car
-except:
-    pass
 from constants import *
+import RPi.GPIO as GPIO
+from gpiozero import DistanceSensor
+
+GPIO.setmode(GPIO.BCM)
 
 # USE MYTRY.PY TO ADJUST YOURSELF WITHIN THE LANE, HAVE PARAMETERS TO BE TWEAKED FOR MAIN TRACK. LATER MAKE PARAMS CENTRAL AND DUALS
-map_radius = 1.3 #m
-max_steer = 40 #deg
-W = 0.2 #m
 
 try:
     car = Car()
-except:
-    pass
+except Exception as e:
+    print(e)
+car.idle()
 
+
+print((math.degrees(math.atan(W/map_radius))/max_steer)*100)
 car.turn((math.degrees(math.atan(W/map_radius))/max_steer)*100)
 car.move()
 
@@ -32,15 +32,22 @@ camera_config = picam2.create_still_configuration(main={"size": (cam_width, cam_
 picam2.configure(camera_config)
 picam2.start()
 i = 0
+sensor = DistanceSensor(echo=ECHO, trigger=TRIG)
 while True:
-    img = picam2.capture_array()
-    img = cv2.rotate(img, cv2.ROTATE_180)
-    outp3 = bw_conv(img)
-    outp4 = crop(outp3)
-    carc = draw(outp4)
-    cv2.imwrite(f"outputimages/{i}-Base.jpg", img)
-    i+=1
-    cv2.waitKey(1)
+    try:
+        if sensor.distance <= min_dist:
+            car.brake()
+        img = picam2.capture_array()
+        outp3 = bw_conv(img)
+        outp4 = crop(outp3)
+        carc = draw(outp4)
+        cv2.imwrite(f"outputimages/{i}-Base.jpg", img)
+        cv2.imwrite(f"outputimages/Processed{i}-{carc[1]}.jpg", carc[0])
+        i+=1
+        cv2.waitKey(1)
+    except KeyboardInterrupt:
+        car.end_car()
+
 
 
 '''
