b76f1f1828ebcada42c104ba18132875bcdb3ec0
Fir121 <mohamedfiras2003@gmail.com>
2023-03-01 22:59:56 +0400

    updated signal reaction, model to be updated

--- Raspberry Pi/Car.py
+++ Raspberry Pi/Car.py
@@ -106,7 +106,7 @@ class Car:
         signal = controls[4]
         zebra = controls[5]
 
-        if "Traffic_X" in signal:
+        if "Traffic_X" in signal and "Traffic_L" not in signal:
             print("Signal detected")
             self.brake()
         elif lane_disp is None:
